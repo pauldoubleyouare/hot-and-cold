3d56c3982939029ec81cdfbeb7a2d8c106d3c069
Paul Rosenberg <paulrosenberg13@gmail.com>
2019-02-27 16:07:47 -0800

    initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import App from './components/App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* 
+
+0. A random number between 1-100 is used as the secret number
+1. User enters a number into an input box
+2. User clicks 'Guess' button
+3. the guessing number gets saved to 'state'
+4. The guessing number gets compared to the secret number - if correct {return WON} else {return 'incorrect}
+5. Input box gets cleared out after each guess
+6. when number is correctly guessed, user can start a new game
+
+
+
+
+*/
